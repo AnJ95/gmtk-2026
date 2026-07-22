3cbfe7c2cca324120bcfc324c6d694d7e9ae6b98
bummelzug <jo8472@proton.me>
2026-07-23 01:06:31 +0200

    item skript

--- item/item.gd
+++ item/item.gd
@@ -1,11 +1,48 @@
-extends Node2D
+extends Sprite2D
 
+@export_group("Time")
+@export var goal: int = 30
+@export var max: int = 40
 
-# Called when the node enters the scene tree for the first time.
+@export_group("Sprites")
+@export var raw: Texture2D
+@export var done: Texture2D
+@export var burnt: Texture2D
+@export var particle_color: Color
+
+var time: int = 0
+var in_microwave: bool = false
+
+enum states {
+		RAW,
+		DONE,
+		BURNT,
+	}
+var state = states.RAW
+
+const THRESHOLD = 1
+
 func _ready() -> void:
-	pass # Replace with function body.
+	self.texture = raw
 
+func enter_microwave():
+	self.in_microwave = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func leave_microwave():
+	self.in_microwave = false
+
+func go_kaputt():
+	queue_free()
+
 func _process(delta: float) -> void:
-	pass
+	if in_microwave:
+		time += delta
+
+	if state == states.RAW and time > goal - THRESHOLD:
+		state = states.DONE
+		self.texture = done
+	elif state == states.DONE and time > goal + THRESHOLD:
+		state = states.BURNT
+		self.texture = burnt
+	elif state == states.BURNT and time > max:
+		go_kaputt()
