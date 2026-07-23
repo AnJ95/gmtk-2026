df722ab6238537b394d9f2313166b08e5008930f
bummelzug <jo8472@proton.me>
2026-07-23 19:36:53 +0200

    Timer is almost working now

--- timer/timer.gd
+++ timer/timer.gd
@@ -1,16 +1,32 @@
 extends Sprite2D
 
-@export var level_time: int = 60
+@export var level_time: int = 20
+
+@export_category("Sound")
+@export var ticks: Array[AudioStream]
+
+var time: float = 0
+var tick_counter: int = 0
+var is_counting: bool = true
 
 func _ready() -> void:
-	$Label.visible_characters = 22
-
-	set_timer(90)
+	tick()
+	set_timer(level_time)
 
 func set_timer(t: int):
 	var tween = create_tween()
 	tween.set_trans(Tween.TRANS_EXPO)
-	tween.tween_property($Label, "visible_characters", 22 + t , 2)
+	tween.tween_property(self, "time", t , 4)
+
+func tick():
+	$Label.visible_characters = int(self.time) + 22
+	$tick.stream = ticks[tick_counter]
+	$tick.play()
+	tick_counter = (tick_counter + 1) % len(ticks)
 
 func _process(delta: float) -> void:
-	pass
+	if $Label.visible_characters != int(time) + 22:
+		tick()
+
+	if is_counting and time >= 0:
+		time -= delta
