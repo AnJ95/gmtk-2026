013277bbe5cd55e466f4b0d2ae042cc40309b167
AnJ95 <angeloaracri@yahoo.de>
2026-07-24 18:55:29 +0200

    Slight improvements scoring/playing animation

--- main/main.gd
+++ main/main.gd
@@ -6,4 +6,5 @@ func _on_level_manager_level_prepare(level_id: int, level: Level) -> void:
 		$AnimationPlayer.play("playing")
 
 func _on_timer_level_end() -> void:
+	await get_tree().create_timer(3).timeout
 	$AnimationPlayer.play("scoring")
