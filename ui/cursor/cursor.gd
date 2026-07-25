be73c1fde296b6f47393f176e1b9747195054fd0
AnJ95 <angeloaracri@yahoo.de>
2026-07-25 02:33:42 +0200

    Revert "Make cursor pos lag"
    This reverts commit e1163d485defe28fa9bc10d3738fe9064bde9d29.

--- ui/cursor/cursor.gd
+++ ui/cursor/cursor.gd
@@ -1,21 +1,13 @@
 extends Node2D
 
-@export var smoothing := 12.0
-
 func _ready() -> void:
 	set_dragging(false)
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	
 func _process(delta: float) -> void:
-	var mouse_pos := get_viewport().get_mouse_position()
-	var viewport_size := get_viewport_rect().size
-
-	var target_pos := Vector2(
-		clamp(mouse_pos.x, 0.0, viewport_size.x),
-		clamp(mouse_pos.y, 0.0, viewport_size.y)
-	)
-
-	global_position = global_position.lerp(target_pos,  1.0 - exp(-smoothing * delta))
+	var mouse_pos = get_viewport().get_mouse_position()
+	global_position.x = clamp(mouse_pos.x, 0, get_viewport_rect().size.x)
+	global_position.y = clamp(mouse_pos.y, 0, get_viewport_rect().size.y)
 
 func set_dragging(dragging: bool):
 	$CursorPoint.visible = !dragging
