6d6c927d2a6497efad2b30dbfc817cf945c82674
bummelzug <jo8472@proton.me>
2026-07-25 03:07:27 +0200

    start and quit

--- menu/menu.gd
+++ menu/menu.gd
@@ -5,3 +5,15 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	pass
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://main/Main.tscn")
+
+func _on_howto_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
