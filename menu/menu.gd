7f759e28db2d15fe1565e2cb505530fbced10697
AnJ95 <angeloaracri@yahoo.de>
2026-07-26 17:59:29 +0200

    Add how-to-play & credits

--- menu/menu.gd
+++ menu/menu.gd
@@ -5,13 +5,20 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_start_pressed() -> void:
+	await move_to_page(0)
 	get_tree().change_scene_to_file("res://main/Main.tscn")
 
 func _on_howto_pressed() -> void:
-	pass # Replace with function body.
+	move_to_page(1)
 
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
+	move_to_page(2)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func move_to_page(i: int):
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	tween.tween_property($Moveable, "position:y", -1080*i, 0.75)
+	await tween.finished
